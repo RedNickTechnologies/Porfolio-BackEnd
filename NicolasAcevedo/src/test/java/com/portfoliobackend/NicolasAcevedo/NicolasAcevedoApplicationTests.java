1dea207ef55d739edcd33aeea2933e2bfa5ed4b9
Nicolas <ngacevedo5@gmail.com>
2022-08-09 21:29:07 -0300

    Primer comitt, inicializando Spring

---
+++ NicolasAcevedo/src/test/java/com/portfoliobackend/NicolasAcevedo/NicolasAcevedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliobackend.NicolasAcevedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolasAcevedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
