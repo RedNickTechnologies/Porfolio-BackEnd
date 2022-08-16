74b7b31a90f622bf71c0efbe18b3190942ba3118
Nicolas <ngacevedo5@gmail.com>
2022-08-16 13:43:35 -0300

    Ajustes menores
    Se realizaron ajustes menores para poder usar la base de datos local

--- NicolasAcevedo/src/main/java/com/portfoliobackend/NicolasAcevedo/Controller/PersonaController.java
+++ NicolasAcevedo/src/main/java/com/portfoliobackend/NicolasAcevedo/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfoliobackend.NicolasAcevedo.Entity.Persona;
 import com.portfoliobackend.NicolasAcevedo.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     
     /* COMANDOS PARA EL POSTMAN (PEDIDOS AL SERVIDOR)*/
@@ -57,5 +59,11 @@ public class PersonaController {
     ipersonaService.savePersona(persona);
     return persona;
     }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
+    }
+
     
 }
